6e705aa14289299d1a5329cafa055a1f0c256120
hbr <helmut.brandl@gmx.net>
2024-03-18 12:12:49 -0600

    Started 'Triemap' with binders
    	modified:   src/simple/triemap.ml

--- src/simple/triemap.ml
+++ src/simple/triemap.ml
@@ -1,4 +1,20 @@
-module String_map = Fmlib_std.Btree.Map (String)
+
+
+
+
+
+(*
+    ============================================================
+    Basics
+    ============================================================
+*)
+
+module String_map =
+  Fmlib_std.Btree.Map (String)
+
+
+module Int_map =
+  Fmlib_std.Btree.Map (Int)
 
 
 module Option =
@@ -11,77 +27,127 @@ struct
 end
 
 
-module Expr =
+type 'a upd =
+  'a -> 'a
+
+
+type 'a xt =
+  'a option upd
+
+
+let (>.>) (f: 'a -> 'b) (g: 'b -> 'c): 'a -> 'c =
+  fun a -> a |> f |> g
+
+
+
+
+
+(*
+    ============================================================
+    Triemap with Terms with Variables and Applications
+    ============================================================
+*)
+module Simple =
 struct
-  type t =
-    | Var of String.t
-    | App of t * t
+  module Expr =
+  struct
+    type t =
+      | Var of String.t
+      | App of t * t
+  end
+
+
+  module Expr_map =
+  struct
+    type 'a t =
+      'a t1 option
+
+    and  'a t1 =
+      {
+        vars: 'a String_map.t;
+        apps: 'a t t
+      }
+
+
+    let vars (n: 'a t1): 'a String_map.t =
+      n.vars
+
+
+    let apps (n: 'a t1): 'a t t =
+      n.apps
+
+
+    let update_vars (f: 'a String_map.t upd): 'a t1 upd =
+      fun n -> {n with vars = f n.vars}
+
+
+    let update_apps (f: 'a t t upd): 'a t1 upd =
+      fun n -> {n with apps = f n.apps}
+
+
+    let empty: 'a t =
+      None
+
+
+    let rec find_opt: type a. Expr.t -> a t -> a option =
+      let open Option
+      in
+      function
+      | Var s ->
+        map vars >=> String_map.find_opt s
+
+      | App (f, a) ->
+        map apps >=> find_opt f >=> find_opt a
+
+
+    let rec update: type a.  Expr.t -> a xt -> a t upd =
+      let open Option
+      in
+      function
+      | Var s ->
+        String_map.update s >.> update_vars >.> map
+
+      | App (f, a) ->
+        update a >.> map >.> update f >.> update_apps >.> map
+  end
 end
 
 
-module Expr_map =
+
+
+
+
+
+
+
+
+(*
+    ============================================================
+    Triemap with Terms with Binders
+    ============================================================
+*)
+module With_binders =
 struct
-  type 'a xt =
-    'a option -> 'a option
+  module Term =
+  struct
+    type t =
+      | Var of int
+      | App of t * t
+      | Lam of t
+  end
 
 
-  type 'a t =
-    'a t1 option
+  module TMap =
+  struct
+    type 'a t =
+      'a t1 option
 
-  and  'a t1 =
-    {
-      vars: 'a String_map.t;
-      apps: 'a t t
-    }
-
-
-  (* 'a t t = 'a t t1 option
-            = 'a t1 option t1 option
-   *)
-
-
-  let vars (n: 'a t1): 'a String_map.t =
-    n.vars
-
-
-  let apps (n: 'a t1): 'a t t =
-    n.apps
-
-
-  let update_vars (f: 'a String_map.t -> 'a String_map.t) (n: 'a t1): 'a t1 =
-    {n with vars = f n.vars}
-
-
-  let update_apps (f: 'a t t -> 'a t t) (n: 'a t1): 'a t1 =
-    {n with apps = f n.apps}
-
-
-  let empty: 'a t =
-    None
-
-
-  let rec find_opt: type a. Expr.t -> a t -> a option =
-    let open Option
-    in
-    function
-    | Var s ->
-      map vars >=> String_map.find_opt s
-
-    | App (f, a) ->
-      map apps >=> find_opt f >=> find_opt a
-
-
-  let (>.>) (f: 'a -> 'b) (g: 'b -> 'c): 'a -> 'c =
-    fun a -> a |> f |> g
-
-
-  let rec update: type a.  Expr.t -> a xt -> a t -> a t =
-    let open Option
-    in
-    function
-    | Var s ->
-      String_map.update s >.> update_vars >.> map
-
-    | App (f, a) ->
-      update a >.> map >.> update f >.> update_apps >.> map
+    and 'a t1 =
+      {
+        bvars: 'a Int_map.t;
+        fvars: 'a Int_map.t;
+        apps:  'a t t;
+        lams:  'a t;
+      }
+  end
 end
